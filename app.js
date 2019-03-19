5b6da0bc19be34327969abe3fb56218a07f33f68
Catalina Meneses <catalina@Catalinas-iMac.local>
2019-03-19 13:07:52 -0500

    Change the connection url

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const product = require('./routes/products.route');
+const bookmark = require('./routes/bookmarks.route');
 // Intialize the express app
 const app = express();
 
 // Database configuration
-let dev_db_url = 'mongodb://kath:kath123@ds117535.mlab.com:17535/maixonproducts';
+let dev_db_url = 'mongodb://kath:kath123@ds117806.mlab.com:17806/bookmarks';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -16,9 +16,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/products', product);
+app.use('/bookmarks', bookmark);
 
-let port = 1234;
+let port = 2772;
 
 app.listen(port, () => {
   console.log('Server is up and running on port numner ' + port);
